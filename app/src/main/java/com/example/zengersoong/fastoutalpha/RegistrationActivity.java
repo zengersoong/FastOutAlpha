cfc47e2f1644d7bb2743674bdb72c9b2cc7d125f
Zenger Soong <zengersoong@gmail.com>
2017-11-21 01:45:51 +0800

    try la

--- app/src/main/java/com/example/zengersoong/fastoutalpha/RegistrationActivity.java
+++ app/src/main/java/com/example/zengersoong/fastoutalpha/RegistrationActivity.java
@@ -12,6 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.quickstart.database.models.User;
 
 public class RegistrationActivity extends AppCompatActivity {
 
